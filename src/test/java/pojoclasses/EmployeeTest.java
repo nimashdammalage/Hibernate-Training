5f978e9928d526bf0832ee088597cc3a5c89886e
Nimash Dilanka <nimash@codegen.net>
2019-07-25 17:44:36 +0530

    c1:difference save() and saveAndUpdate()

--- src/test/java/pojoclasses/EmployeeTest.java
+++ src/test/java/pojoclasses/EmployeeTest.java
@@ -40,7 +40,16 @@ public class EmployeeTest extends TestCase
             Employee employee = new Employee( "hemal", "nilanga", 4500 );
             Address address = new Address( "firstLane", "secondLne", "Sri lanka" );
             employee.setAddress( address );
+            /*
+            *Main difference between save and saveOrUpdate method is that save() generates a new identifier and INSERT record into database while saveOrUpdate can either INSERT or UPDATE based upon existence of record.
+            * Clearly saveOrUpdate is more flexible in terms of use but it involves an extra processing to find out whether record already exists in table or not.
+            * in summery  save() method saves records into database by INSERT SQL query, Generates a new identifier and return the Serializable identifier back.
+            *On the on other hand  saveOrUpdate() method either INSERT or UPDATE based upon existence of object in database. If persistence object already exists in database
+            * then UPDATE SQL will execute and if there is no corresponding object in database than INSERT will run
+            * */
             int empId = (int) session.save( employee );
+            session.saveOrUpdate( employee ); //returns void
+
             transaction.commit();
 
             /*Update operation*/
